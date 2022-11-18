e8db4ffec7d5992afc53dd2d0ce3427ca1c4331e
Wandonium <hillarywando@gmail.com>
2022-11-18 14:18:17 +0300

    Form complete with sql inserts

--- app/public/config/database.php
+++ app/public/config/database.php
@@ -11,4 +11,4 @@
     if($conn->connect_error) {
         die('Connection Failed ' . $conn->connect_error);
     }
-
+?>
--- app/public/inc/header.php
+++ app/public/inc/header.php
@@ -1,5 +1,4 @@
 <?php include 'config/database.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
